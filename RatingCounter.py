e0451fbb185387b8f1567f896a61bf459e9039a9
tantd9 <tranduytan.220300@gmail.com>
2024-02-09 14:58:20 +0700

    MRFrinedsByAge   :bee:

--- RatingCounter.py
+++ RatingCounter.py
@@ -3,10 +3,10 @@ from mrjob.job import MRJob
 class MRRatingCounter(MRJob):
     def mapper(self, key, line):
         (userID, movieID, rating, timestamp) = line.split('\t')
-        yield rating, 1
-
+        yield rating, 1 
+#  yield return 1 list trong đó key là rating  còn value  = 1  
     def reducer(self, rating, occurences):
         yield rating, sum(occurences)
-
+#occurences  lần xuất hiện
 if __name__ == '__main__':
     MRRatingCounter.run()
